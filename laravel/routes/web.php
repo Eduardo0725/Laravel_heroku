89a0dc351c688ff600d7227fc414dd40d878a183
Eduardo0725 <eduardom0142@gmail.com>
2020-06-17 19:02:31 -0300

    Aula Laravel e PostgreSQL no Heroku

---
+++ laravel/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/userlist', function () { $umUsuarioQualquer = new User; $umUsuarioQualquer->name = 'aUserName';
+
+    $timestamp = date("Y-m-d-h-i-sa"); $umUsuarioQualquer->email = "adsf@asdf.com".$timestamp;
+
+    $umUsuarioQualquer->password = '123'; $umUsuarioQualquer->save(); return User::all();
+});
